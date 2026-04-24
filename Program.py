55fab01659152549bcb7f51b401ebd39658c67b8
Haydee Delgado <dhelgado723@gmail.com>
2026-04-23 20:47:45 -0500

    edad,altura,nombre y true

--- Program.py
+++ Program.py
@@ -1,2 +1,16 @@
 print("Hola mundo")
 print("Saludos")
+
+edad: int = 18
+altura: float = 1.49
+nombre: str = "Haydee"
+esRojo: bool = True
+
+print(f"{nombre} tiene {edad} años y mide {altura} y su vestido {"es"if esRojo else "no es"} rojo")
+
+"""
+# result = "Mayor edad" if edad > 18 else "Niño"
+result = edad > 18 ? "Mayor edad":"Niño";
+dddd
+ffff
+"""
